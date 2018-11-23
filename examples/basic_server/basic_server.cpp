2babd738984bd92475b4b2cc32f635645a87aeb1
Kian Nejadfard <k.nejadfard@gmail.com>
2018-11-23 18:04:33 -0500

    Add basic_server example application

---
+++ examples/basic_server/basic_server.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "PROJECT_C/PROJECT_C.hpp"
+#include "handler.hpp"
+
+int main(int argc, char* argv[]) {
+
+    try {
+        PROJECT_C::router r;
+        r.get("/api/hello", handler{});
+        PROJECT_C::server s("127.0.0.1", 8001);
+        s.router(r);
+        s.listen();
+    }
+    catch (std::exception const& e) {
+        PROJECT_C::main_logger.error("Fatal error: " + std::string{e.what()});
+        return EXIT_FAILURE;
+    }
+
+    return 0;
+}
+
---
+++ examples/basic_server/handler.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <map>
+#include <fstream>
+#include <sstream>
+#include "PROJECT_C/PROJECT_C.hpp"
+
+
+class handler {
+
+public:
+
+    handler()
+    {
+    }
+
+    void operator()(PROJECT_C::request_parser& request, PROJECT_C::response_writer& response_writer)
+    {
+        PROJECT_C::main_logger.info("Handling request: " + std::string(request));
+        std::map<std::string, std::string> params = request.query_params();
+        for(std::map<std::string, std::string>::const_iterator it = params.cbegin(); it != params.cend(); ++it) {
+            PROJECT_C::main_logger.debug("Param: " + it->first + "=" + it->second);
+        }
+
+        std::string content_type = request.content_type();
+        if(content_type == PROJECT_C::constants::octet_stream) {
+            std::ofstream out("output.txt");
+            out << request.stream().rdbuf();
+            out.close();
+        }
+
+        PROJECT_C::response_builder res;
+        res.result(PROJECT_C::http::status::ok);
+        res.body("Hello World!");
+        response_writer.write(res.build());
+    }
+};
