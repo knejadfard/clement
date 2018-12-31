561e29ba9d7695d4fab2c63b0439a8ac112a9357
Kian Nejadfard <k.nejadfard@gmail.com>
2018-12-31 16:47:56 -0500

    Add helper build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ./build
+mkdir build
+pushd build
+
+CC=~/binaries/llvm-7.0.0/bin/clang CXX=~/binaries/llvm-7.0.0/bin/clang++ \
+    cmake -DBOOST_ROOT=/home/kian/binaries/boost-1.68-clang ../
+
+make -j 4
+
+popd
