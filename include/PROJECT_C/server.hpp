a55d9c4f6c6db9ff081705ae555b6692111db478
Kian Nejadfard <k.nejadfard@gmail.com>
2019-02-03 18:07:00 -0500

    Separate function declarations from definitions in server
    In order to cleanup the interface of this class, function definitions
    are separated from declarations. The end result is a cleaner class
    interface. Both declarations and definitions still reside in the
    header file.

--- include/PROJECT_C/server.hpp
+++ include/PROJECT_C/server.hpp
@@ -48,101 +48,110 @@ public:
             router_{} {
     }
 
-    void listen() {
-        main_logger.info("Started listening on " +
-                address_ + ":" + std::to_string(port_));
+    // TODO comments
+    void listen();
 
-        boost::asio::io_context ioc{1};
+    // TODO comments
+    void work();
 
-        auto const address = boost::asio::ip::make_address(address_);
-
-        tcp::acceptor acceptor{ioc, {address, port_}};
-        for(;;) {
-            tcp::socket socket{ioc};
-
-            acceptor.accept(socket);
-
-            std::unique_lock<std::mutex> lock(mutex_);
-            incoming_requests_.push(std::move(socket));
-            main_logger.debug("Added new socket to queue");
-            lock.unlock();
-
-            handler_condition_.notify_all();
-        }
-    }
-
-    void work() {
-        while(true) {
-            std::unique_lock<std::mutex> lock(mutex_);
-            // block thread until there are items to process
-            handler_condition_.wait(lock,
-                    [this](){ return !incoming_requests_.empty(); });
-
-            boost::asio::ip::tcp::socket socket = std::move(
-                    incoming_requests_.front());
-            incoming_requests_.pop();
-            main_logger.debug("Picked a new socket from queue");
-
-            lock.unlock();
-
-            response_writer writer(socket);
-
-            request_parser request;
-            try {
-                request.parse(socket);
-            }
-            catch(std::exception& e) {
-                std::string msg = "Error parsing request: " +
-                        std::string(e.what());
-                main_logger.error(msg);
-                http::response<http::string_body> response{
-                        http::status::internal_server_error, 1};
-                response.body() = msg;
-                writer.write(std::move(response));
-                socket.shutdown(socket_type::shutdown_send);
-                continue;
-            }
-
-            boost::beast::http::verb verb = request.verb();
-            main_logger.debug("Received request: " + std::string(request));
-            main_logger.debug("Body size: " + std::to_string(
-                    request.stream().str().size()));
-
-            router::handler_type handler;
-            try {
-                route requested_route{request.verb_string(), request.target()};
-                handler = router_.get_handler(requested_route);
-                route mapped_route = router_.get_route(requested_route);
-                request.mapped_route(mapped_route);
-                handler(request, writer);
-            }
-            catch(std::out_of_range& e) {
-                std::string msg = "Requested target cannot be found: [" +
-                        request.verb_string() + "] " + request.target();
-                main_logger.error(msg);
-                http::response<http::string_body> response{
-                        http::status::not_found, 1};
-                response.body() = msg;
-                writer.write(std::move(response));
-            }
-            catch(std::exception& e) {
-                main_logger.error("Error when handling request [" +
-                        request.verb_string() + "] " + request.target() +
-                        " : " + std::string{e.what()});
-                http::response<http::string_body> response{
-                        http::status::internal_server_error, 1};
-                response.body() = e.what();
-                writer.write(std::move(response));
-            }
-
-            socket.shutdown(socket_type::shutdown_send);
-        }
-    }
-
-    void set_router(router& r) {
-        router_ = r;
-    }
+    // TODO comments
+    void set_router(router& r);
 
 };
+
+void server::listen() {
+    main_logger.info("Started listening on " +
+            address_ + ":" + std::to_string(port_));
+
+    boost::asio::io_context ioc{1};
+
+    auto const address = boost::asio::ip::make_address(address_);
+
+    tcp::acceptor acceptor{ioc, {address, port_}};
+    for(;;) {
+        tcp::socket socket{ioc};
+
+        acceptor.accept(socket);
+
+        std::unique_lock<std::mutex> lock(mutex_);
+        incoming_requests_.push(std::move(socket));
+        main_logger.debug("Added new socket to queue");
+        lock.unlock();
+
+        handler_condition_.notify_all();
+    }
+}
+
+void server::work() {
+    while(true) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        // block thread until there are items to process
+        handler_condition_.wait(lock,
+                [this](){ return !incoming_requests_.empty(); });
+
+        boost::asio::ip::tcp::socket socket = std::move(
+                incoming_requests_.front());
+        incoming_requests_.pop();
+        main_logger.debug("Picked a new socket from queue");
+
+        lock.unlock();
+
+        response_writer writer(socket);
+
+        request_parser request;
+        try {
+            request.parse(socket);
+        }
+        catch(std::exception& e) {
+            std::string msg = "Error parsing request: " +
+                    std::string(e.what());
+            main_logger.error(msg);
+            http::response<http::string_body> response{
+                http::status::internal_server_error, 1};
+            response.body() = msg;
+            writer.write(std::move(response));
+            socket.shutdown(socket_type::shutdown_send);
+            continue;
+        }
+
+        boost::beast::http::verb verb = request.verb();
+        main_logger.debug("Received request: " + std::string(request));
+        main_logger.debug("Body size: " + std::to_string(
+                request.stream().str().size()));
+
+        router::handler_type handler;
+        try {
+            route requested_route{request.verb_string(), request.target()};
+            handler = router_.get_handler(requested_route);
+            route mapped_route = router_.get_route(requested_route);
+            request.mapped_route(mapped_route);
+            handler(request, writer);
+        }
+        catch(std::out_of_range& e) {
+            std::string msg = "Requested target cannot be found: [" +
+                    request.verb_string() + "] " + request.target();
+            main_logger.error(msg);
+            http::response<http::string_body> response{
+                    http::status::not_found, 1};
+            response.body() = msg;
+            writer.write(std::move(response));
+        }
+        catch(std::exception& e) {
+            main_logger.error("Error when handling request [" +
+                    request.verb_string() + "] " + request.target() +
+                    " : " + std::string{e.what()});
+            http::response<http::string_body> response{
+                    http::status::internal_server_error, 1};
+            response.body() = e.what();
+            writer.write(std::move(response));
+        }
+
+        socket.shutdown(socket_type::shutdown_send);
+    }
+}
+
+inline void server::set_router(router& r) {
+    router_ = r;
+}
 
 } // namespace PROJECT_C
