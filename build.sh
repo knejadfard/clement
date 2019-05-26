7c651b1b48fde12da45345105ab06abb6d80238a
Kian Nejadfard <k.nejadfard@gmail.com>
2019-05-26 14:04:13 -0400

    Update CC/CXX env vars to llvm 8

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -rf ./build
 mkdir build
 pushd build
 
-CC=~/binaries/llvm-7.0.0/bin/clang CXX=~/binaries/llvm-7.0.0/bin/clang++ \
+CC=~/binaries/llvm-8.0.0/bin/clang CXX=~/binaries/llvm-8.0.0/bin/clang++ \
     cmake -DBOOST_ROOT=/home/kian/binaries/boost-1.68-clang ../
 
 make -j 4
